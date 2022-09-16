337d1c11474490ed9e48fee934fbebecd1db7206
Fran Lurbe <francisco.lurbe@gmail.com>
2022-09-16 15:00:19 +0200

    Algunas pruebas mas

--- CallbackHell/app.js
+++ CallbackHell/app.js
@@ -1,17 +1,62 @@
-const willYouGetDog = new Promise((resolve, reject) => {
-    const result = Math.random();
-    if(result < 0.5){
-        resolve();
-    }
-    else {
-        reject();
-    }
-});
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            const rand = Math.random();
+            if(rand<0.3){
+                reject();
+            }
+            else {
+                resolve();
+            }
+        },3000);
+    })
+}
 
-willYouGetDog.then(() => {
-    'Entro por el then.';
-})
+fakeRequest()
+    .then(()=>{console.log('Request worked')})
+    .catch(()=>{console.log('Request failed')});
 
-willYouGetDog.catch(()=>{
-    'Entro por el catch.'
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const makeDogPromise = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(()=>{
+//             const result = Math.random();
+//             if(result < 0.5) {
+//                 resolve();
+//             }
+//             else {
+//                 reject();
+//             }
+//         }, 3000)
+//     });
+// };
+
+// makeDogPromise()
+//     .then(() => {
+//         console.log('Entro por el then.');
+//     })
+//     .catch(() => {
+//         console.log('Entro por el catch.');
+//     });
+
