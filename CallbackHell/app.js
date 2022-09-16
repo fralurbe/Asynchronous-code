8b4e48b321a15cd679c1c612fbc6d95168c3bf17
Fran Lurbe <francisco.lurbe@gmail.com>
2022-09-16 12:35:58 +0200

    promesas

--- CallbackHell/app.js
+++ CallbackHell/app.js
@@ -1,58 +1,17 @@
-const btn = document.querySelector('button');
-
-// setTimeout(() => {
-//     btn.style.transform = 'translateX(100px)';
-//     setTimeout(() => {
-//         btn.style.transform = 'translateX(200px)';
-//         setTimeout(() => {
-//             btn.style.transform = 'translateX(300px)';
-//             setTimeout(() => {
-//                 btn.style.transform = 'translateX(400px)';
-//             },1000)
-//         },1000)
-//     },1000)
-// },1000)
-
-const moveX = (element, amount, delay, onSuccess, onFailure) => {
-    const bodyBoundary = document.body.clientWidth;
-    const elRight = element.getBoundingClientRect().right;
-    const currLeft = element.getBoundingClientRect().left;
-    if(elRight + amount > bodyBoundary) {
-        onFailure();
+const willYouGetDog = new Promise((resolve, reject) => {
+    const result = Math.random();
+    if(result < 0.5){
+        resolve();
     }
-    else{
-        setTimeout(() => {
-            element.style.transform = `translateX(${currLeft +  amount}px)`;
-            onSuccess();
-        }, delay);
+    else {
+        reject();
     }
-};
+});
 
-// moveX(btn, 50, 500, ()=> {
-//     moveX(btn, 50, 500, ()=> {
-//         moveX(btn, 50, 500, ()=> {
-//             moveX(btn, 50, 500, ()=> {
-//                 moveX(btn, 50, 500, ()=> {
-//                 });
-//             });
-//         });
-//     });
-// });
+willYouGetDog.then(() => {
+    'Entro por el then.';
+})
 
-moveX(
-        btn, 
-        50, 
-        500,
-        () => {
-            moveX(
-                btn,
-                50,
-                500,
-                ()=>{console.log('Seguimos moviendo')},
-                ()=>{console.log('Hemos llegado al final')}
-            )
-        },
-        () => {        
-            alert('He llegado al final')
-        }
-    )
+willYouGetDog.catch(()=>{
+    'Entro por el catch.'
+})
